75b807f631843fef2238ee323fc881e3a956a856
Luis Eduardo Fernandez Troncoso <113368001+LuisFernandez-Ksquare@users.noreply.github.com>
2023-02-01 20:38:15 -0400

    Final

--- force-app/main/default/lwc/accountContacts/accountContacts.js
+++ force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -7,39 +7,38 @@ const PAGE_SIZE = 5;
 export default class ContactSearchComponent extends LightningElement {
   @track searchKey = '';
   @track contacts = [];
+  @track fields = ['Name', 'Email', 'Phone'];
+  @track createNewContact = false;
+  @track pageNumber = 1;
+  @track totalPages;
   columns = [
     { label: 'Name', fieldName: 'Name' },
     { label: 'Email', fieldName: 'Email', type: 'email' },
     { label: 'Phone', fieldName: 'Phone', type: 'phone' }
   ];
-  @track page = {
-    number: 1,
-    size: PAGE_SIZE,
-    totalPages: 0,
-  };
-  @track createNewContact = false;
 
   handleSearchKeyChange(event) {
     this.searchKey = event.target.value;
   }
 
-  async searchContacts() {
-    try {
-      const result = await searchContacts({
-        searchKey: this.searchKey,
-        pageNumber: this.page.number,
-        pageSize: this.page.size
+  searchContacts() {
+    searchContacts({
+      searchKey: this.searchKey,
+      pageNumber: this.pageNumber,
+      pageSize: PAGE_SIZE
+    })
+      .then(result => {
+        this.contacts = result.contacts;
+        this.totalPages = Math.ceil(result.totalNumberOfContacts / PAGE_SIZE);
+      })
+      .catch(error => {
+        console.error(error);
       });
-      this.contacts = result.contacts;
-      this.page.totalPages = Math.ceil(result.totalNumberOfContacts / this.page.size);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
   handleSuccess(event) {
     this.contacts.push(event.detail.fields);
-    this.createNewContact = false;
+    this.createNewcontact = false;
   }
 
   createContact() {
@@ -51,18 +50,20 @@ export default class ContactSearchComponent extends LightningElement {
   }
 
   previousPage() {
-    if (this.page.number > 1) {
-      this.page.number--;
+    if (this.pageNumber > 1) {
+      this.pageNumber--;
       this.searchContacts();
     }
   }
 
   nextPage() {
-    if (this.page.number < this.page.totalPages) {
-      this.page.number++;
+    if (this.pageNumber < this.totalPages) {
+      this.pageNumber++;
       this.searchContacts();
     }
   }
 }
+
+
 
 
